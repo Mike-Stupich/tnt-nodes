8771373f6e0db2b8cf344b5751cded32e9e45147
Mike Stupich <mike.stupich@gmail.com>
2017-09-23 02:22:57 -0400

    A few more initial commits and sample connections

--- sshWrapper.ts
+++ sshWrapper.ts
@@ -1,6 +1,23 @@
 import SSH from 'simple-ssh'
 import * as fs from 'fs'
 
-const createConnections = (filePath?:string) => {
+const DEFAULT_FILE_PATH = './resources/'
 
+const readFile = (filePath:string) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', ((err, data) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve (data)
+            }
+        }))
+    })
+}
+
+const createConnections = (filePath?:string) => {
+    //let 
+    if (filePath) {
+       // fs.readFile(FilePath)
+    }
 }
